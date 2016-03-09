9d74b8a7818eca49f1be29de3404124d98c86616
Ariel T. Glenn <ariel@wikimedia.org>
2016-03-09 23:53:27 +0200

    add script for updating cow images, for use with gitbuilder container

---
+++ jessie/gitbuilds/updatecows.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/usr/sbin/cowbuilder --update --basepath /var/cache/pbuilder/base-jessie-amd64.cow
+/usr/sbin/cowbuilder --update --basepath /var/cache/pbuilder/base-trusty-amd64.cow
+/usr/sbin/cowbuilder --update --basepath /var/cache/pbuilder/base-precise-amd64.cow
