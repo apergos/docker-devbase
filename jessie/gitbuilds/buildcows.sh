3c0cd14b887452db0924260821f99c752049e7e5
Ariel T. Glenn <ariel@wikimedia.org>
2016-03-09 23:51:08 +0200

    fix type in script that bulds cow images, was broken for precise

--- jessie/gitbuilds/buildcows.sh
+++ jessie/gitbuilds/buildcows.sh
@@ -9,4 +9,4 @@ cp /root/precise/D01apt.wikimedia.org /var/cache/pbuilder/hooks/precise
 
 /usr/sbin/cowbuilder --create --mirror http://mirrors.wikimedia.org/ubuntu --distribution trusty --components "main universe" --architecture amd64 --basepath /var/cache/pbuilder/base-trusty-amd64.cow --debootstrapopts --variant=buildd --debootstrapopts --keyring=/usr/share/keyrings/ubuntu-archive-keyring.gpg
 
-/usr/sbin/cowbuilder --create --mirror http://mirrors.wikimedia.org/ubuntu --distribution precise --components main "universe" --architecture amd64 --basepath /var/cache/pbuilder/base-precise-amd64.cow --debootstrapopts --variant=buildd --debootstrapopts --keyring=/usr/share/keyrings/ubuntu-archive-keyring.gpg
+/usr/sbin/cowbuilder --create --mirror http://mirrors.wikimedia.org/ubuntu --distribution precise --components "main universe" --architecture amd64 --basepath /var/cache/pbuilder/base-precise-amd64.cow --debootstrapopts --variant=buildd --debootstrapopts --keyring=/usr/share/keyrings/ubuntu-archive-keyring.gpg
